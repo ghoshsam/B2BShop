b75dc390b4f1d7d3b60768f12bc5111c8f9f8e3a
insyncsam <samarendra@insync.co.in>
2017-10-05 11:09:34 +0530

    Update

---
+++ B2B.Data/AbstractRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2B.Data
+{
+    public class AbstractRepository<T> : IRepository<T> where T : class
+    {
+        public T Get(Func<T, bool> predicate)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Func<T, bool> predicate = null)
+        {
+            throw new NotImplementedException();
+        }
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Attach(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+      
+    }
+}
---
+++ B2B.Data/UnitOfWork.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace B2B.Data
+{
+    public class UnitOfWork : IDisposable
+    {
+        private SampleDbEntities entities = null;
+
+        public GenericUnitOfWork()
+        {
+            entities = new SampleDbEntities();
+        }
+
+        public Dictionary<Type, object> repositories = new Dictionary<Type, object>();
+
+        public IRepository<T> Repository<T>() where T : class
+        {
+            if (repositories.Keys.Contains(typeof(T)) == true)
+            {
+                return repositories[typeof(T)] as IRepository<T>
+            }
+            IRepository<T> repo = new GenericRepository<T>(entities);
+            repositories.Add(typeof(T), repo);
+            return repo;
+        }
+
+        public void SaveChanges()
+        {
+            entities.SaveChanges();
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!this.disposed)
+            {
+                if (disposing)
+                {
+                    entities.Dispose();
+                }
+            }
+            this.disposed = true;
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+    }
+}
